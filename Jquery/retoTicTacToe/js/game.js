79c5d68cef78a4385dd752f29c183c388d8bfd10
Tevan <jezuluaga16@misena.edu.co>
2015-08-12 20:24:57 -0500

    Reto Tic Tac Toe

--- Jquery/retoTicTacToe/js/game.js
+++ Jquery/retoTicTacToe/js/game.js
@@ -12,7 +12,6 @@ $(document).ready(function(){
 	$('article#error').hide();
 	$('section#screen2').hide();
 	$('section#screen3').hide();
-	$('section#screen3 h1').hide()
 	$('section#screen1').animate({'left': '-1000px'},0)
 						.animate({'left': '40px'},500)
 						.animate({'left': '0px'},200);
@@ -82,27 +81,198 @@ $('#boxes').on('click', 'button', function(){
 	if($countslt%2==0){
 		$('#turno_jug').text($nplayer1);
 		$(this).css('background', '#2085ac url(imgs/'+$splayer2+'.png) no-repeat center center');
-		$(this).addClass('p2');
+		$(this).addClass('pl2');
 	}else{
 		$('#turno_jug').text($nplayer2);
 		$(this).css('background', '#2085ac url(imgs/'+$splayer1+'.png) no-repeat center center');
-		$(this).addClass('p1');
+		$(this).addClass('pl1');
 	}
 	$(this).addClass('activebox');
 	$(this).attr('disabled', 'disabled');
 	$countslt++;
-
 	checkGame();
 });
 
-function checkGame(p1, p2){
-	for ($i = 0; $winner.length; $i++) {
-		for ($j = 0; $j < 3; $j++) {
-			console.log($winner[$i][$j]);
+//Hover de los botones cuando entra
+$('#boxes').on('mouseenter', 'button', function(){
+		if($countslt%2==0){
+			$(this).css('background', '#2085ac url(imgs/'+$splayer2+'.png) no-repeat center center');
+		}else{
+			$(this).css('background', '#2085ac url(imgs/'+$splayer1+'.png) no-repeat center center');
 		}
-	}
-}
+})
+// Hove de los Botones cuando sale
+$('#boxes').on('mouseleave', 'button', function(){
+		if($countslt%2==0){
+			$(this).css('background', '#2085ac');
+		}else{
+			$(this).css('background', '#2085ac');
+		}
+})
 
+function checkGame(){
+	//Jugadas Ganadoras del Player 2--------------
+	if($('button#box1').hasClass('pl2') && $('button#box2').hasClass('pl2') && $('button#box3').hasClass('pl2')){
+		$('button#box1').css('background-color','green');
+		$('button#box2').css('background-color','green');
+		$('button#box3').css('background-color','green');
+		$('span#playAgain').text("¿Volver a Jugar?");
+		ganadorPlayer2();
+		ganarJuego();
+	}
+	else if ($('button#box4').hasClass('pl2') && $('button#box5').hasClass('pl2') && $('button#box6').hasClass('pl2')){
+		$('button#box4').css('background-color','green');
+		$('button#box5').css('background-color','green');
+		$('button#box6').css('background-color','green');
+		$('span#playAgain').text("¿Volver a Jugar?");
+		ganadorPlayer2();
+		ganarJuego();
+	}
+	else if ($('button#box7').hasClass('pl2') && $('button#box8').hasClass('pl2') && $('button#box9').hasClass('pl2')){
+		$('button#box7').css('background-color','green');
+		$('button#box8').css('background-color','green');
+		$('button#box9').css('background-color','green');
+		alert('ha ganado');
+		ganarJuego();
+	}
+	else if ($('button#box1').hasClass('pl2') && $('button#box4').hasClass('pl2') && $('button#box7').hasClass('pl2')){
+		$('button#box1').css('background-color','green');
+		$('button#box4').css('background-color','green');
+		$('button#box7').css('background-color','green');
+		$('span#playAgain').text("¿Volver a Jugar?");
+		ganadorPlayer2();
+		ganarJuego();
+	}
+	else if ($('button#box2').hasClass('pl2') && $('button#box5').hasClass('pl2') && $('button#box8').hasClass('pl2')){
+		$('button#box2').css('background-color','green');
+		$('button#box5').css('background-color','green');
+		$('button#box8').css('background-color','green');
+		$('span#playAgain').text("¿Volver a Jugar?");
+		ganadorPlayer2();
+		ganarJuego();
+	}
+	else if ($('button#box3').hasClass('pl2') && $('button#box6').hasClass('pl2') && $('button#box9').hasClass('pl2')){
+		$('button#box3').css('background-color','green');
+		$('button#box6').css('background-color','green');
+		$('button#box9').css('background-color','green');
+		$('span#playAgain').text("¿Volver a Jugar?");
+		ganadorPlayer2();
+		ganarJuego();
+	}
+	else if ($('button#box1').hasClass('pl2') && $('button#box5').hasClass('pl2') && $('button#box9').hasClass('pl2')){
+		$('button#box1').css('background-color','green');
+		$('button#box5').css('background-color','green');
+		$('button#box9').css('background-color','green');
+		$('span#playAgain').text("¿Volver a Jugar?");
+		ganadorPlayer2();
+		ganarJuego();
+	}
+	else if ($('button#box3').hasClass('pl2') && $('button#box5').hasClass('pl2') && $('button#box7').hasClass('pl2')){
+		$('button#box3').css('background-color','green');
+		$('button#box5').css('background-color','green');
+		$('button#box7').css('background-color','green');
+		$('span#playAgain').text("¿Volver a Jugar?");
+		ganadorPlayer2();
+		ganarJuego();
+	}
+//Jugadas Ganadoras del Player 1--------------
+else if($('button#box1').hasClass('pl1') && $('button#box2').hasClass('pl1') && $('button#box3').hasClass('pl1')){
+	$('button#box1').css('background-color','green');
+	$('button#box2').css('background-color','green');
+	$('button#box3').css('background-color','green');
+	$('span#playAgain').text("¿Volver a Jugar?");
+	ganadorPlayer1();
+	ganarJuego();
+}
+else if ($('button#box4').hasClass('pl1') && $('button#box5').hasClass('pl1') && $('button#box6').hasClass('pl1')){
+	$('button#box4').css('background-color','green');
+	$('button#box5').css('background-color','green');
+	$('button#box6').css('background-color','green');
+	$('span#playAgain').text("¿Volver a Jugar?");
+	ganadorPlayer1();
+	ganarJuego();
+}
+else if ($('button#box7').hasClass('pl1') && $('button#box8').hasClass('pl1') && $('button#box9').hasClass('pl1')){
+	$('button#box7').css('background-color','green');
+	$('button#box8').css('background-color','green');
+	$('button#box9').css('background-color','green');
+	$('span#playAgain').text("¿Volver a Jugar?");
+	ganadorPlayer1();
+	ganarJuego();
+}
+else if ($('button#box1').hasClass('pl1') && $('button#box4').hasClass('pl1') && $('button#box7').hasClass('pl1')){
+	$('button#box1').css('background-color','green');
+	$('button#box4').css('background-color','green');
+	$('button#box7').css('background-color','green');
+	$('span#playAgain').text("¿Volver a Jugar?");
+	ganadorPlayer1();
+	ganarJuego();
+}
+else if ($('button#box2').hasClass('pl1') && $('button#box5').hasClass('pl1') && $('button#box8').hasClass('pl1')){
+	$('button#box2').css('background-color','green');
+	$('button#box5').css('background-color','green');
+	$('button#box8').css('background-color','green');
+	$('span#playAgain').text("¿Volver a Jugar?");
+	ganadorPlayer1();
+	ganarJuego();
+}
+else if ($('button#box3').hasClass('pl1') && $('button#box6').hasClass('pl1') && $('button#box9').hasClass('pl1')){
+	$('button#box3').css('background-color','green');
+	$('button#box6').css('background-color','green');
+	$('button#box9').css('background-color','green');
+	$('span#playAgain').text("¿Volver a Jugar?");
+	ganadorPlayer1();
+	ganarJuego();
+}
+else if ($('button#box1').hasClass('pl1') && $('button#box5').hasClass('pl1') && $('button#box9').hasClass('pl1')){
+	$('button#box1').css('background-color','green');
+	$('button#box5').css('background-color','green');
+	$('button#box9').css('background-color','green');
+	$('span#playAgain').text("¿Volver a Jugar?");
+	ganadorPlayer1();
+	ganarJuego();
+}
+else if ($('button#box3').hasClass('pl1') && $('button#box5').hasClass('pl1') && $('button#box7').hasClass('pl1')){
+	$('button#box3').css('background-color','green');
+	$('button#box5').css('background-color','green');
+	$('button#box7').css('background-color','green');
+	$('span#playAgain').text("¿Volver a Jugar?");
+	ganadorPlayer1();
+	ganarJuego();
+}
+};
+
+function ganadorPlayer2(){
+	$('article#error').show();
+	$('article#error').css('background-image','url(imgs/winner.png)');
+	$('article#error h1').text('El ganador es: '+' '+$nplayer2);
+	$('article#error h2').text('');
+	$('article#error').css({'background-image':'url(imgs/winner.png)','background-color':'rgba(255,255,255, 0.8)'});
+	$('article#error h1').css('margin','290px auto');
+	$('article#error h2').css({
+		'background': '#176381 url(imgs/'+$splayer2+'.png) no-repeat center center',
+		'width':'90px',
+		'height':'90px',
+		'position':'relative',
+		'top':'-250px',
+		'left':'640px',
+});
+}
+function ganadorPlayer1(){
+	$('article#error').show();
+	$('article#error h1').text('El ganador es: '+' '+$nplayer1);
+	$('article#error h2').text('');
+	$('article#error').css({'background-image':'url(imgs/winner.png)','background-color':'rgba(255,255,255, 0.8)'});
+	$('article#error h1').css('margin','290px auto');
+	$('article#error h2').css({
+		'background': '#176381 url(imgs/'+$splayer1+'.png) no-repeat center center',
+		'width':'90px',
+		'height':'90px',
+		'position':'relative',
+		'top':'-250px',
+		'left':'640px',
+});
+}
 function ganarJuego(){
 	$('nav#boxes button').attr('disabled', 'disabled');
 }
